875f54e940561e7c359ddb8757f678fea3b49560
amonjerro <amonjerro@hotmail.com>
2021-07-22 12:20:51 -0500

    Barebones

---
+++ index.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+from pymongo import MongoClient
+import os
+
+app = Flask(__name__)
+uri = os.environ['uri']
+client = MongoClient(uri)
+
+
+@app.route('/post',methods=['POST'])
+def insert():
+    params = request.form
+    db = client['database']
+    col = db['tweets']
+    col.insert(params)
+    return True,200
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', PORT=PORT)
